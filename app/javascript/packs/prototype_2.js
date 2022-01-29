7206028d2aced7d688c72ef65f7c22c74b85773d
ZakharDay <zakhar.day@gmail.com>
2022-01-29 17:29:16 +0300

    Prototype 2 finished

--- app/javascript/packs/prototype_2.js
+++ app/javascript/packs/prototype_2.js
@@ -1,5 +1,47 @@
-import { test } from '../prototype_2/script'
+const colors = ['#00FF29', '#79FF39', '#CFCFCF', '#C5C5C5', '#8D8D8D']
+const deepfaceQuantity = [2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18, 20]
 
-console.log('prototype 2 loaded')
+const deepfaceTypes = [
+  'deepface-green-white',
+  'deepface-green-black',
+  'deepface-dark-green-white',
+  'deepface-dark-green-black',
+  'deepface-black'
+]
 
-test()
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function createCircle(frame) {
+  const circleElement = document.createElement('div')
+  circleElement.classList.add('circle')
+
+  const top = getRandomArbitrary(-100, 1720)
+  const left = getRandomArbitrary(-100, 980)
+  const size = getRandomArbitrary(10, 500)
+
+  circleElement.style.top = [top, 'px'].join('')
+  circleElement.style.left = [left, 'px'].join('')
+  circleElement.style.width = [size, 'px'].join('')
+  circleElement.style.height = [size, 'px'].join('')
+  circleElement.style.transform = `rotate(${getRandomArbitrary(10, 350)}deg)`
+  circleElement.classList.add(sample(deepfaceTypes))
+
+  frame.appendChild(circleElement)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementsByClassName('prototype_2')[0]
+  const frame = document.createElement('div')
+  frame.classList.add('frame')
+  container.appendChild(frame)
+
+  for (var i = 0; i < sample(deepfaceQuantity); i++) {
+    createCircle(frame)
+  }
+})
--- app/javascript/prototype_2/script.js
+++
@@ -1,5 +0,0 @@
-function test() {
-  console.log('test passed')
-}
-
-export { test }
