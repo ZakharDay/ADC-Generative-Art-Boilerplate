084b82879937d54c5daa7cff832f68f14336e6a2
ZakharDay <zakhar.day@gmail.com>
2022-01-29 17:12:03 +0300

    First prototype finished

--- app/javascript/packs/prototype_1.js
+++ app/javascript/packs/prototype_1.js
@@ -1,5 +1,37 @@
-import { test } from '../prototype_1/script'
+const colors = ['#00FF29', '#79FF39', '#CFCFCF', '#C5C5C5', '#8D8D8D']
 
-console.log('prototype 1 loaded')
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min
+}
 
-test()
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function createCircle(frame) {
+  const circleElement = document.createElement('div')
+  circleElement.classList.add('circle')
+
+  const top = getRandomArbitrary(-100, 1720)
+  const left = getRandomArbitrary(-100, 980)
+  const size = getRandomArbitrary(10, 500)
+
+  circleElement.style.top = [top, 'px'].join('')
+  circleElement.style.left = [left, 'px'].join('')
+  circleElement.style.width = [size, 'px'].join('')
+  circleElement.style.height = [size, 'px'].join('')
+  circleElement.style.backgroundColor = sample(colors)
+
+  frame.appendChild(circleElement)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_1')
+  const frame = document.createElement('div')
+  frame.classList.add('frame')
+  container.appendChild(frame)
+
+  for (var i = 0; i < 50; i++) {
+    createCircle(frame)
+  }
+})
